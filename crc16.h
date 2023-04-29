0ccb8fff3ae355a6f96b8ee0f911cadc8f852437
Adarsh dhananajaya k <76220527+Adarsh-dhananjaya-k@users.noreply.github.com>
2023-04-29 10:46:05 +0530

    Add files via upload

---
+++ crc16.h
@@ -0,0 +1,9 @@
+#ifndef _CRC_16_H
+#define _CRC_16_H
+
+
+
+unsigned short CRC16 (const unsigned char *nData, unsigned short wLength);
+//unsigned short gen_crc16(const unsigned char *data, unsigned short size);
+
+#endif
